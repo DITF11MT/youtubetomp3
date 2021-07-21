01958fb1a7d23c2e4a1d59675647b9ed645090a1
Talha <Tal7a.tmam@gmail.com>
2021-07-22 00:41:16 +0300

    Hellooooo I is back

--- api/server.js
+++ api/server.js
@@ -2,28 +2,44 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const ytdl = require('ytdl-core');
-
+const fs = require('fs');
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.set('views', __dirname);
+
+app.use(express.static(__dirname + '/public'));
+
+app.engine('html', require('ejs').renderFile);
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('index.html'));
+
 
 app.post('/', async (req, res) => {
     try {
         const { url } = req.body;
-        res.set('Content-Disposition', `attachment; filename="Song.mp3"`);
-        await ytdl(url, {
-            format: 'mp3',
-            quality: 'highestaudio'
-        }).pipe(res);
+        let info = await ytdl.getInfo(url);
+        info = info.videoDetails.media;
+        console.log(info);
+        const dir = 'songs/';
+        let fullName = 'song.mp3';
+        if(!Object.entries(info).length == 0)
+         fullName = info.song + '—' + info.artist + '.mp3'
+        ytdl(url, {
+            filter: 'audioonly'
+        })
+        .pipe(fs.createWriteStream(dir + fullName));
+
     }catch (err) {
         res.status(400).json('Error Happend!');
     }
 });
-
-app.listen(process.env.PORT || 3001, ()=> {
-  console.log(`app is running on port ${process.env.PORT}`);
+const port = process.env.PORT || 3001;
+app.listen(port, ()=> {
+  console.log(`app is running on port ${port}`);
 })
